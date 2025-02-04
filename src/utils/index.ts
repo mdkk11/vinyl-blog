da2be8e1229eac5252212aa18fb1261e0e15467c
mk <mk.eng1119@gmail.com>
2025-02-04 19:49:51 +0900

    add: add util functions

---
+++ src/utils/index.ts
@@ -0,0 +1,41 @@
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...classes: ClassValue[]) {
+  return twMerge(clsx(classes))
+}
+
+export function assertType<T extends string>(
+  type: T,
+  obj: { type: string },
+): asserts obj is { type: T } {
+  if (obj.type !== type) {
+    throw new Error(`unexpected type: ${obj.type}`)
+  }
+}
+
+export function isExist<T>(v: T | null | undefined): v is NonNullable<T> {
+  return typeof v !== 'undefined' && v !== null
+}
+
+export function assertIsExist<T>(
+  v: T | null | undefined,
+  target = '',
+): asserts v is NonNullable<T> {
+  if (!isExist(v)) {
+    throw new Error(`${target} should be specified`.trim())
+  }
+}
+
+export function isObject(v: unknown): v is Record<string, unknown> {
+  return typeof v === 'object' && v !== null
+}
+
+export function assertIsObject(
+  v: unknown,
+  target = '',
+): asserts v is Record<string, unknown> {
+  if (!isObject(v)) {
+    throw new Error(`${target} should be object`.trim())
+  }
+}
